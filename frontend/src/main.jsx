410da16f0c9b5dbca3e486f714b96dd9594a9a1d
Falolu Henry <henryfalolu1@gmail.com>
2025-02-26 17:44:47 +0100

    Add initial server implementation, update main entry point, and modify styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import TaskManager from './App.jsx'
+import Spline from "@splinetool/react-spline/next";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TaskManager />
-  </StrictMode>,
+  <Spline scene="https://prod.spline.design/g492ldEQrkUfS7zK/scene.splinecode" />
+    <TaskManager /></StrictMode>
 )
+
+
