f76c5ef9371f69f04c1f74565301070d16022386
Falolu Henry <henryfalolu1@gmail.com>
2025-02-26 16:14:45 +0100

    Refactor TaskManager component and update styles for improved UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@
 
 export default function TaskManager() {
   return (
-    <main>
-      <h1>Manage your daily tasks</h1>
-      <p>Team and Project management with solution providing App</p>
+    <>
+      <main>
+        <h1>Manage your daily tasks</h1>
+        <p>Team and Project management with solution providing App</p>
 
-      <div>
-        Get Started
-      </div>
-    </main>
-   
-  )
+        
+         <a href="./task.jsx" className="getStarted"><span className="getSpan">Get</span> Started</a> 
+        
+      </main>
+    </>
+  );
 }
