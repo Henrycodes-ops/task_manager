2bbf3f4e8a391ef1dd493ae3faea5a97ae602414
Falolu Henry <henryfalolu1@gmail.com>
2025-02-27 10:40:43 +0100

    Refactor TaskManager component to use SplineContainer and update layout styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
 // import { useState } from "react";
-import Spline from "@splinetool/react-spline";
+
+
+import SplineContainer from "./spline";
+import Task from "./task";
 
 export default function TaskManager() {
   return (
     <>
       <main className="appContainer">
-        <div className="splineContainer">
-          <Spline scene="https://prod.spline.design/g492ldEQrkUfS7zK/scene.splinecode" />
-        </div>
-
         <div className="header">
-            <h1>Manage your daily tasks</h1>
-        <p>Team and Project management with solution providing App</p>
-
-        <a href="./task.jsx" className="getStarted">
-          <span className="getSpan">Get</span> Started
-        </a>
+          <h1>Manage your daily tasks</h1>
+          <p>Team and Project management with solution providing App</p>
+          <a href="./task.jsx" className="getStarted">
+            <span className="getSpan">Get</span> Started
+          </a>
         </div>
 
-      
+        <SplineContainer />
       </main>
     </>
   );
---
+++ frontend/src/spline.jsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline";
+
+export default function SplineContainer() {
+  return (
+    <div className="splineContainer">
+      <Spline scene="https://prod.spline.design/g492ldEQrkUfS7zK/scene.splinecode" />
+    </div>
+  );
+}
