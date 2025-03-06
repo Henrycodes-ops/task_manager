eadc5c908df5bde1ab16bf78dd4723a5a7286ba4
Falolu Henry <henryfalolu1@gmail.com>
2025-03-06 10:52:50 +0100

    Refactor routing in App component to use Routes and add Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./header";
 import Task from "./task";
 import SplineContainer from "./spline";
+import Home from "./home";
 
 export default function TaskManager() {
   return (
-    <main className="appContainer">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/task" component={Task} />
-        </Switch>
-        <SplineContainer />
-      </Router>
-    </main>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main className="appContainer">
+              <Header />
+              <Home />
+              <SplineContainer />
+            </main>
+          }
+        />
+        <Route path="/task" element={<Task />} />
+      </Routes>
+    </Router>
   );
 }
