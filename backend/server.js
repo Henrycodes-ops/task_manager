639a9590d01bb039b07fa983c64595a6156af34a
Falolu Henry <henryfalolu1@gmail.com>
2025-03-08 22:46:21 +0100

    Remove backend.env file and comment out dotenv and server setup in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,39 @@
-dotenv.config();
+// dotenv.config();
 
-import path from "path";
-import { fileURLToPath } from "url";
-import express from "express";
-import dotenv from "dotenv";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import express from "express";
+// import dotenv from "dotenv";
 
-dotenv.config();
-const app = express();
+// dotenv.config();
+// const app = express();
 
 console.log("Server is running...");
 
 // Convert __dirname to work with ES6 modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 // Define the port number for the server to listen on
-const port = process.env.PORT || 3000;
+const port = 5000;
 
 console.log(`Server is running on http://localhost:${port}`);
 // Serve static files from the "src" directory
-app.use(express.static(path.join(__dirname, "frontend", "src")));
-app.use(express.static(path.join(__dirname, "src")));
+// app.use(express.static(path.join(__dirname, "frontend", "src")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "src", "index.html"));
-//   console.log(res);
 
-// });
 
-const sendAppFile = (req, res) => {
-  console.log("Sending App.jsx file...");
-  const appPath =
-    process.env.APP_PATH || path.join(__dirname, "frontend", "src", "App.jsx");
-  res.sendFile(appPath, (err) => {
-    if (err) {
-      console.error("Error sending file:", err);
-    } else {
-      console.log("App.jsx file sent successfully!");
-    }
-  });
-};
 
-app.get("/", sendAppFile);
+// const sendAppFile = (req, res) => {
+//   console.log("Sending App.jsx file...");
+//   const appPath =
+//      path.join( __dirname, "./frontend/src/app.jsx");
+//   res.sendFile(appPath, (err) => {
+//     if (err) {
+//       console.error("Error sending file:", err);
+//     } else {
+//       console.log("App.jsx file sent successfully!");
+//     }
+//   });
+// };
+
+// app.get("/", sendAppFile);
