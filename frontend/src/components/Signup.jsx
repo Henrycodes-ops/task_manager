36488861d92149570725b3d4ef9aba6dbd096107
Falolu Henry <henryfalolu1@gmail.com>
2025-03-10 21:48:30 +0100

    Add SplineBlob component to Signup page and enhance CSS for layout and styling

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,16 @@
+import { SplineBlob } from "./spline";
+
 export default function Login() {
   return (
-    <main>
-      
+    <><main>
+      <SplineBlob />
     </main>
+      <div className="signup">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis est fugit mollitia delectus quo modi eos vitae, nostrum officiis numquam dolores vero placeat quas. Sit laboriosam qui provident architecto et.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis est fugit mollitia delectus quo modi eos vitae, nostrum officiis numquam dolores vero placeat quas. Sit laboriosam qui provident architecto et.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis est fugit mollitia delectus quo modi eos vitae, nostrum officiis numquam dolores vero placeat quas. Sit laboriosam qui provident architecto et.
+      </div>
+    </>
+    
   )
 }
