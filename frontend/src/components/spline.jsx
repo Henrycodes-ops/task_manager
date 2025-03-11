f84c2dbeeefdb15d1dea1372210c8253376cc053
Falolu Henry <henryfalolu1@gmail.com>
2025-03-11 15:36:19 +0100

    Enhance SplineContainer with loading state management and update background color in CSS for improved aesthetics

--- frontend/src/components/spline.jsx
+++ frontend/src/components/spline.jsx
@@ -1,19 +1,30 @@
+import React, { useState, createContext, useContext, useEffect } from "react";
+
 import Spline from "@splinetool/react-spline";
 
+const SplineContext = createContext({ loaded: false });
+
 export default function SplineContainer() {
+  const [loaded, setLoaded] = useState(false);
+  const { setSplineLoaded } = useContext(SplineLoadContext);
+
+  const handleSplineLoad = () => {
+    setLoaded(true);
+    setSplineLoaded(true);
+  };
+
   return (
     <div className="splineContainer">
-      <Spline scene="https://prod.spline.design/Yja0XAhwPaIh2nCb/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/Yja0XAhwPaIh2nCb/scene.splinecode" onLoad={handleSplineLoad}/>
     </div>
   );
 }
 
 export function SplineBlob() {
+
   return (
     <div className="splineBlob">
-       <Spline
-        scene="https://prod.spline.design/KT77YMuJyvvFZfgQ/scene.splinecode" 
-      />
+      <Spline scene="https://prod.spline.design/KT77YMuJyvvFZfgQ/scene.splinecode" />
     </div>
   );
 }
